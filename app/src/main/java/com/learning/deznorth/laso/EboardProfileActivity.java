175e9ca8a61c84c60a0380282aa4abbf5ec0f185
David Rojas Gonzalez <deznorth@gmail.com>
2017-08-01 22:25:12 -0400

    Added class EboardProfileActivity Added layout activity_eboard_profile Created a copy of activity_profile to test new interface without compromising original

---
+++ app/src/main/java/com/learning/deznorth/laso/EboardProfileActivity.java
@@ -0,0 +1,17 @@
+package com.learning.deznorth.laso;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dezno on 8/1/2017.
+ */
+
+public class EboardProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eboard_profile);
+    }
+}
